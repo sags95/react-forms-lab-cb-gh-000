83cc9b7c15dd09c9918aba60e08e78f4efd4975f
Alex Sagel <alex.sagel7587@gmail.com>
2018-01-09 02:46:46 +0000

    Done.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -4,24 +4,104 @@ class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+      valid: false,
+    };
+
   }
 
+  onChange = event => {
+    var lineCheck = false;
+
+    this.setState({
+      value: event.target.value,
+    })
+
+//line count functionality
+    var lines = event.target.value;
+    var lineCount = lines.split(/\r\n|\r|\n/).length
+    if (lineCount == 3){
+      lineCheck = true;
+    }
+
+
+//word tokenizer
+    var lineArr = '';
+    lineArr = lines.split('\n');
+
+    var firstLine = lineArr[0];
+    var fCheck = false;
+    if (firstLine.split(' ').length == 5){
+      fCheck = true;
+    }
+
+    var secLine = lineArr[1];
+    var sCheck = false;
+    if (secLine != null){
+    if (secLine.split(' ').length == 3){
+      sCheck = true;
+    }
+  }
+
+    var thrLine = lineArr[2];
+    if (thrLine != null){
+    var tCheck = false;
+    if (thrLine.split(' ').length == 5){
+      tCheck = true;
+    }
+  }
+
+    if (fCheck && sCheck && tCheck && lineCheck == true){
+      this.setState({
+        valid: true,
+      })
+    } else {
+      this.setState({
+        valid: false,
+      })
+    }
+
+  //  console.log(firstLine, secLine, thrLine);
+
+  }
+
+
+
   render() {
-    return (
-      <div>
+
+    if (this.state.valid == false){
+      return (
+        <div>
+          <textarea
+            rows="3"
+            cols="60"
+            name="text"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <div
+            id="poem-validation-error"
+            style={{color: 'red'}}
+          >
+            This poem is not written in the right structure!
+          </div>
+        </div>
+      );
+    } else {
+      return(
+        <div>
         <textarea
           rows="3"
           cols="60"
-        />
-        <div
-          id="poem-validation-error"
-          style={{color: 'red'}}
-        >
-          This poem is not written in the right structure!
-        </div>
-      </div>
-    );
+          name="text"
+          value={this.state.text}
+          onChange={this.onChange}
+          />
+          </div>
+      );
+    }
+
   }
 }
 
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,40 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+
+    this.state = {
+      value: '',
+      remChars: 140,
+      chars: 0
+    };
+    this.charCounter = this.charCounter.bind(this);
+
+  }
+
+  charCounter = event => {
+    const max = this.props.maxChars;
+    var input = event.target.value;
+
+    this.setState({
+      value: event.target.value,
+      chars: input.length,
+      remChars: max - this.state.chars
+
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text"
+          value={this.state.value}
+          onChange={this.charCounter}
+        />
+        <div>
+        <strong> Chars left: {this.state.remChars}</strong>
+        <strong> Total chars: {this.state.chars}</strong>
+        </div>
       </div>
     );
   }
